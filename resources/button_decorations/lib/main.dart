531ccd710c09460fdfa88c6dc9ea5f6f2fc21933
Abdullah <abdullahsh123@hotmail.com>
2021-06-17 05:03:46 +0500

    box decorations

--- resources/button_decorations/lib/main.dart
+++ resources/button_decorations/lib/main.dart
@@ -42,7 +42,15 @@ class _State extends State<MyApp> {
           children: <Widget>[
            new Slider(value: _value, onChanged: _onchanged,min: 0.0,max: 4.0,),
             new RotatedBox(quarterTurns: turns,
-            child: new Text("Hello"),)
+            child: new Text("Sunshine"),),
+            new Container(
+              child: new Image(image: new AssetImage('images/simle.png')),
+              padding: new EdgeInsets.all(32.0),
+              decoration: new BoxDecoration(
+                border: new Border.all(color: Colors.orange,width: 2.0),
+                gradient: new RadialGradient(colors: <Color>[Colors.red,Colors.yellow]),
+              ),
+            )
           ],
         ),
       ),
